96b56cf22e6eaf8b2b905daac77a06a517c308ee
Murtaxa-Baig <darvaish49@gmail.com>
2023-10-04 13:41:58 +0500

    Fetch follower of user

--- app/github/page.js
+++ app/github/page.js
@@ -1,6 +1,7 @@
 "use client"
 import React from 'react'
 import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { useState } from 'react';
 import style from '@/styles/github/github.module.css'
 
@@ -8,6 +9,7 @@ export default function Github() {
 
     const [userData, setUserData] = useState(null);
     const [userName, setUserName] = useState('')
+    const [followers, setFollowers] = useState([])
     const [err, setErr] = useState(null)
     const getUserData = async () => {
         try {
@@ -16,13 +18,22 @@ export default function Github() {
             console.log("Console responce", responce.data);
             setUserName('')
             setErr(null)
+            setFollowers([])
         }
         catch (error) {
             setErr('User name is invalid')
             setUserName('')
-            userData(null)
+            setUserData(null)
+            setFollowers([])
         }
     }
+    const fecthUserFollowers = async () => {
+        let responce = await axios.get(userData.followers_url)
+        console.log(responce.data)
+        setFollowers(responce.data)
+
+    }
+    console.log("foolower console", followers)
 
     return (
         <>
@@ -36,26 +47,51 @@ export default function Github() {
                     value={userName}
                     className={style.inputField}
                 />
-                {err && <p>{err}</p> }
-                <h1>
-                    <button
-                        onClick={getUserData}
-                        className={style.btn}
-                    >Click for detail</button>
-                </h1>
+                {err && <p>{err}</p>} <br />
+                <button
+                    onClick={getUserData}
+                    className={`mt-2 ${style.btn}`}
+                >Search</button>
             </div>
             {
                 userData &&
 
                 <div className={style.displayDiv}>
+                    <p><img src={userData?.avatar_url} alt="" className={style.profile_img} /></p>
                     <p>UserNAme: {userData?.login}</p>
                     <p>Name: {userData?.name}</p>
-                    <p>Followers: {userData?.followers}</p>
+                    <p>Bio: {userData?.bio}</p>
+                    <p><button
+                        onClick={fecthUserFollowers}
+                        className={style.follower_btn}
+                    >Followers: </button>{userData?.followers}</p>
                     <p>Following: {userData?.following}</p>
                     <p>Public repo: {userData?.public_repos}</p>
                     <p>Created at: {userData?.created_at}</p>
                 </div>
             }
+
+            <div className='container mt-2'>
+                <div className="row">
+                    {
+                        followers.length > 0 &&
+                        followers.map((item, index) => {
+                            return (
+                                <>
+                                    <div
+                                        key={index}
+                                        className={`col-md-2 col-sm-6 m-2 p-2 ${style.card}`}>
+                                        <img src={item.avatar_url} alt="" className={style.followerImg} />
+                                        <p className='mt-2'>{item.login}</p>
+                                    </div>
+
+                                </>
+                            )
+                        })
+                    }
+                </div>
+            </div >
+
         </>
     )
 }
